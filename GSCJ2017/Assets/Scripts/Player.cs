5291e381f78f182ddcb2b938bffe35885acf2e62
Tredwood <tom.redwood@live.co.uk>
2017-05-11 17:09:00 +0100

    Player Prefab and script

---
+++ GSCJ2017/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * (speed));
+    }
+
+    void Update()
+    {
+
+    }
+}
+
