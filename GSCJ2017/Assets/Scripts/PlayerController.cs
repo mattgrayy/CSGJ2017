6bcfbca4dedbb16da234ac228da10ac2a5757774
Tredwood <tom.redwood@live.co.uk>
2017-05-12 12:10:57 +0100

    Player rotation on movement

--- GSCJ2017/Assets/Scripts/PlayerController.cs
+++ GSCJ2017/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,16 @@ public class PlayerController : MonoBehaviour {
     public int playerIndex;
     public bool spawned = false;
 
+    public float rightRot = -10;
+    public float leftRot = 10;
+    float target = 0;
+
     private Player player;
     private Rigidbody rb;
     [SerializeField]
     private GameObject canvas;
+    [SerializeField]
+    private GameObject spawnPoint;
 
     void Start()
     {
@@ -21,13 +27,15 @@ public class PlayerController : MonoBehaviour {
         foreach (MeshRenderer m in GetComponentsInChildren<MeshRenderer>())
             m.enabled = false;
         spawned = false;
-        canvas.SetActive(false); 
+        canvas.SetActive(false);
+        transform.position = spawnPoint.transform.position;
 
     }
 
     void FixedUpdate()
     {
-        if(player.GetButtonDown("Start"))
+        
+        if (player.GetButtonDown("Start"))
         {
             if (spawned)
             {
@@ -35,6 +43,7 @@ public class PlayerController : MonoBehaviour {
                     m.enabled = false;
                 canvas.SetActive(false);
                 spawned = false;
+                transform.position = spawnPoint.transform.position;
             }
             else
             {
@@ -46,13 +55,25 @@ public class PlayerController : MonoBehaviour {
         }
         if (spawned)
         {
+            Vector3 currentRotation = transform.eulerAngles;
+           // float zTarget = Mathf.Round(currentRotation.z / 18) * 180;
+
+            currentRotation.z = Mathf.LerpAngle(currentRotation.z, target, Time.deltaTime * 4);
+            transform.eulerAngles = currentRotation;
+
             if (player.GetButton("Right"))
             {
                 rb.AddForce((Vector3.right * (speed)) * Time.deltaTime);
+                target = rightRot;
             }
             if (player.GetButton("Left"))
             {
                 rb.AddForce((-Vector3.right * (speed)) * Time.deltaTime);
+                target = leftRot;
+            }
+            if(!player.GetButton("Right") && !player.GetButton("Left"))
+            {
+                target = 0;
             }
         }
     }
