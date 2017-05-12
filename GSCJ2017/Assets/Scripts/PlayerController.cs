7c3af1df5fc06e89b615a742b0565ecbc792787f
Tredwood <tom.redwood@live.co.uk>
2017-05-12 11:19:49 +0100

    Player drop in system basics

--- GSCJ2017/Assets/Scripts/PlayerController.cs
+++ GSCJ2017/Assets/Scripts/PlayerController.cs
@@ -6,23 +6,54 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 25.0f;
     public int playerIndex;
+    public bool spawned = false;
 
     private Player player;
     private Rigidbody rb;
+    [SerializeField]
+    private GameObject canvas;
 
     void Start()
     {
         player = ReInput.players.GetPlayer(playerIndex);
         Debug.Log(player);
         rb = GetComponent<Rigidbody>();
+        foreach (MeshRenderer m in GetComponentsInChildren<MeshRenderer>())
+            m.enabled = false;
+        spawned = false;
+        canvas.SetActive(false); 
+
     }
 
     void FixedUpdate()
     {
-        if (player.GetButton("Right"))
+        if(player.GetButtonDown("Start"))
         {
-            Debug.Log("blep");
-            rb.AddForce((Vector3.right * (speed))* Time.deltaTime);
+            if (spawned)
+            {
+                foreach (MeshRenderer m in GetComponentsInChildren<MeshRenderer>())
+                    m.enabled = false;
+                canvas.SetActive(false);
+                spawned = false;
+            }
+            else
+            {
+                foreach (MeshRenderer m in GetComponentsInChildren<MeshRenderer>())
+                    m.enabled = true;
+                spawned = true;
+                canvas.SetActive(true);
+            }
+        }
+        if (spawned)
+        {
+            if (player.GetButton("Right"))
+            {
+                rb.AddForce((Vector3.right * (speed)) * Time.deltaTime);
+            }
+            if (player.GetButton("Left"))
+            {
+                rb.AddForce((-Vector3.right * (speed)) * Time.deltaTime);
+            }
         }
     }
 
