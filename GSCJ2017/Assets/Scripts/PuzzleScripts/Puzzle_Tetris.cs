b053a3e8e96c145d3d00c935799e0d96175256f7
Dreggrim <Jake2.King@live.uwe.ac.uk>
2017-05-12 18:51:12 +0100

    Posers and the last of the models

---
+++ GSCJ2017/Assets/Scripts/PuzzleScripts/Puzzle_Tetris.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle_Tetris : Puzzle {
+
+
+
+
+	[SerializeField] RectTransform spinner;
+
+	void Update ()
+	{
+
+		//puzzle in here
+
+		if (player.GetButtonDown("Right"))
+		{
+
+            spinner.transform.Rotate(new Vector3(0, 0, 45f));
+
+		}
+
+        if (player.GetButtonDown("Left"))
+        {
+
+            spinner.transform.Rotate(new Vector3(0, 0, -45f));
+
+        }
+        
+    }
+}
