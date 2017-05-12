2339476db0805db529611b44132e9f0fcaac75c6
Tredwood <tom.redwood@live.co.uk>
2017-05-12 10:14:55 +0100

    Set up 8 player rewired stuff

---
+++ GSCJ2017/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 25.0f;
+    public int playerIndex;
+
+    private Player player;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(playerIndex);
+        Debug.Log(player);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (player.GetButton("Right"))
+        {
+            Debug.Log("blep");
+            rb.AddForce((Vector3.right * (speed))* Time.deltaTime);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
+
