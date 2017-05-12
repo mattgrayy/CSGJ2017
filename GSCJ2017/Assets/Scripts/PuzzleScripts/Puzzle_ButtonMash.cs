05062f30a74b925c97b039e9768727f850923495
Tredwood <tom.redwood@live.co.uk>
2017-05-12 17:12:44 +0100

    Button mash puzzle, without visuals

---
+++ GSCJ2017/Assets/Scripts/PuzzleScripts/Puzzle_ButtonMash.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle_ButtonMash : Puzzle {
+
+    bool switched = false;
+    int mashed = 0;
+
+    [SerializeField]
+    RectTransform button;
+
+    void Update()
+    {
+        if (player.GetButtonDown("Interact") && !switched)
+        {
+            mashed++;
+            if(mashed == 20)
+            {
+                switched = true;
+                completePuzzle(true);
+            }
+
+            // should make animation but...
+
+        }
+    }
+}
