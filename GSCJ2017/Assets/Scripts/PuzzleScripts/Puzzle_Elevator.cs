25293a84029f5f77941afbd0b3bf037dc5c872d1
OwenRuddle <Owen2.Ruddle@live.uwe.ac.uk>
2017-05-12 20:00:29 +0100

    trying to get floor number working. sorta do

--- GSCJ2017/Assets/Scripts/PuzzleScripts/Puzzle_Elevator.cs
+++ GSCJ2017/Assets/Scripts/PuzzleScripts/Puzzle_Elevator.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class Puzzle_Elevator : Puzzle{
     [SerializeField]  int floorchoice;
+    [SerializeField]Text floornum;
+    bool started = false;
 	// Use this for initialization
 	void Start () {
-       // floorchoice = myCreator.GetComponent<Elevator>().currentLOC;
+        floorchoice = myCreator.GetComponent<Elevator>().currentLOC;
+       // floornum.text = floorchoice.ToString();
+        started = false;
     }
 	
 	// Update is called once per frame
@@ -13,42 +19,53 @@ public class Puzzle_Elevator : Puzzle{
         if(player != null)
         {
            
-            if ( player.GetButtonDown("Up"))
-        // if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            floorchoice += 1 ;
-               
-                if (floorchoice > 3)
-            {
-                floorchoice = 3;
-            }
-        }
+                if (player.GetButtonDown("Up"))
+                {
+                    floorchoice += 1;
 
-        if (player.GetButtonDown("Left"))
-        //if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            floorchoice -= 1;
-                
-            if (floorchoice < 0)
-            {
-                floorchoice = 0;
-            }
-               
-        }
-            if (player.GetButtonDown("Interact"))
-          
-            {
+                    if (floorchoice > 2)
+                    {
+                        floorchoice = 3;
+                    }
+                    floornum.text = floorchoice.ToString();
+                }
 
-                
-              
-                myCreator.GetComponent<Elevator>().ell = floorchoice;
-                myCreator.GetComponent<Elevator>().callel = true;
-                   
-                    completePuzzle(true);
-                   
-                
+                if (player.GetButtonDown("Left"))
 
+                {
+                    floorchoice -= 1;
+
+                    if (floorchoice < 0)
+                    {
+                        floorchoice = 0;
+                    }
+                    floornum.text = floorchoice.ToString();
+                }
+                Debug.Log(started);
+            
+   
+
+
+                    if (player.GetButtonDown("Interact"))
+
+                    {
+                started = true;
+                if (started)
+                {
+
+                    myCreator.GetComponent<Elevator>().ell = floorchoice;
+                    myCreator.GetComponent<Elevator>().callel = true;
+                }
+                completePuzzle(true);
+                Debug.Log(started);
+               // floornum.text = myCreator.GetComponent<Elevator>().currentLOC.ToString();
             }
+                    
+       
         }
     }
 }
+           
+        
+    
+
