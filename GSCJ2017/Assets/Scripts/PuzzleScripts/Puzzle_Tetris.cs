993dc3c6aa6a8d8d1c4d093bb849cfe6c962ca95
Dreggrim <Jake2.King@live.uwe.ac.uk>
2017-05-13 10:47:55 +0100

    Posters and start to tetris puzzle

--- GSCJ2017/Assets/Scripts/PuzzleScripts/Puzzle_Tetris.cs
+++ GSCJ2017/Assets/Scripts/PuzzleScripts/Puzzle_Tetris.cs
@@ -1,12 +1,74 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Puzzle_Tetris : Puzzle {
 
+    class SpinnerPosition
+    {
+        public bool complete;
+
+        public int numberOfHitsLeft, shapeNeeded;
+
+        
+    }
 
 
 
-	[SerializeField] RectTransform spinner;
+    [SerializeField] int spinnerPosition  = 1;
+    
+	[SerializeField] RectTransform spinner = null;
+
+
+    List<SpinnerPosition> Positions = new List<SpinnerPosition>();
+    public  List<int> shapes = new List<int>();
+    public List<int> TempList = new List<int>();
+
+    public int dificulty = 1;
+
+
+    void Start()
+    {
+
+        //set up the first shapes
+      for (int i = 1; i < 5; i++)
+        {
+
+            SpinnerPosition pos = new SpinnerPosition();
+            pos.shapeNeeded = i;
+            pos.numberOfHitsLeft = dificulty;
+
+            Positions.Add(pos);
+
+        }
+
+       foreach (SpinnerPosition spinPos in Positions)
+        {
+            for (int i= 0; i< spinPos.numberOfHitsLeft; i++)
+            {
+
+                shapes.Add(spinPos.shapeNeeded);
+                
+            }
+            
+        }
+
+       
+
+       foreach (int shape in shapes)
+        {
+
+            TempList.Insert(Random.Range(0, shapes.Count), shape);
+        }
+
+
+       foreach (int index in TempList)
+        {
+            
+        }
+
+
+    }
+
 
 	void Update ()
 	{
@@ -17,7 +79,7 @@ public class Puzzle_Tetris : Puzzle {
 		{
 
             spinner.transform.Rotate(new Vector3(0, 0, 45f));
-
+            
 		}
 
         if (player.GetButtonDown("Left"))
@@ -26,6 +88,24 @@ public class Puzzle_Tetris : Puzzle {
             spinner.transform.Rotate(new Vector3(0, 0, -45f));
 
         }
+
+
+        if (player.GetButton("Interact"))
+        {
+
+            //drop a shape from the list
+            
+            //if it matches the position of the spinner then 
+
+        }
+
+
+
+
         
     }
+
+
+   
+
 }
---
+++ GSCJ2017/Assets/lightChnager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightChnager : MonoBehaviour
+{
+
+
+    public  Material Red, Green, Blue, Yellow, White;
+
+    float timer;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        timer += Time.deltaTime;
+
+
+        //the time could be related to the amout of stuff that is borken
+        //will need to get it rom whatever manger that is going to be made
+
+        if (timer > 2)
+        {
+
+            int lightInt = Random.Range(0, 5);
+
+            switch (lightInt)
+            {
+                case 0:
+                    spriteRenderer.material = Red;
+                    break;
+
+                case 1:
+                    spriteRenderer.material = Green;
+                    break;
+
+                case 2:
+                    spriteRenderer.material = Blue;
+                    break;
+
+                case 3:
+                    spriteRenderer.material = Yellow;
+                    break;
+
+                case 4:
+                    spriteRenderer.material = White;
+                    break;
+
+            }
+            timer = 0f;
+
+        }
+
+    }
+}
